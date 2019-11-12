54110ba25ffc74c01e2e8a1ec16b15474596a2b2
kbaba1001 <kbaba1001@users.noreply.github.com>
2019-11-13 04:39:21 +0900

    run graphql-yoga

--- index.ts
+++ index.ts
@@ -1,14 +1,37 @@
-import { prisma } from './generated/prisma-client'
+// import { prisma } from './generated/prisma-client'
 
-// A `main` function so that we can use async/await
-async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+// // A `main` function so that we can use async/await
+// async function main() {
+//   // Create a new user called `Alice`
+//   const newUser = await prisma.createUser({ name: 'Alice' })
+//   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log(allUsers)
+//   // Read all users from the database and print them to the console
+//   const allUsers = await prisma.users()
+//   console.log(allUsers)
+// }
+
+// main().catch(e => console.error(e))
+
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type Definition
+const typeDefs = `
+type Query {
+  character: String!
+}
+`
+
+// Resolvers
+const resolvers = {
+  Query: {
+    character: () => `The force is strong with this API!`
+  }
 }
 
-main().catch(e => console.error(e))
+// Server
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
